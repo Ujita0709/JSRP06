c4c437c41277a9fe7243848f75156c3ab5c18124
Hiroki_ujita <hiroki_ujita@hotmail.com>
2025-12-10 11:32:23 +0900

    main.jsx,App.jsx追加

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import  Counter  from "./Counter.jsx";
+
+function App() {
+  
+  return (
+   <div>
+     <h1>React状態管理練習</h1> 
+      <Counter />
+        
+  </div>
+  );
+}
+
+export default App
